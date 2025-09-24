6648a3944216e44996e0793c1c16a21597dcc522
Yiwen He(何翌闻) <2548124362@qq.com>
2025-09-24 15:12:31 +0800

    add

--- assets/js/data/mathjax.js
+++ assets/js/data/mathjax.js
@@ -1,3 +1,28 @@
+// ---
+// layout: compress
+// # WARNING: Don't use '//' to comment out code, use '{% comment %}' and '{% endcomment %}' instead.
+// ---
+
+// {%- comment -%}
+//   See: <https://docs.mathjax.org/en/latest/options/input/tex.html#tex-options>
+// {%- endcomment -%}
+
+// MathJax = {
+//   tex: {
+//     {%- comment -%} start/end delimiter pairs for in-line math {%- endcomment -%}
+//     inlineMath: [
+//       ['$', '$'],
+//       ['\\(', '\\)']
+//     ],
+//     {%- comment -%} start/end delimiter pairs for display math {%- endcomment -%}
+//     displayMath: [
+//       ['$$', '$$'],
+//       ['\\[', '\\]']
+//     ],
+//     {%- comment -%} equation numbering {%- endcomment -%}
+//     tags: 'ams'
+//   }
+// };
 ---
 layout: compress
 # WARNING: Don't use '//' to comment out code, use '{% comment %}' and '{% endcomment %}' instead.
@@ -8,18 +33,25 @@ layout: compress
 {%- endcomment -%}
 
 MathJax = {
+  loader: { load: ['[tex]/physics',] },
   tex: {
-    {%- comment -%} start/end delimiter pairs for in-line math {%- endcomment -%}
     inlineMath: [
       ['$', '$'],
       ['\\(', '\\)']
     ],
-    {%- comment -%} start/end delimiter pairs for display math {%- endcomment -%}
     displayMath: [
       ['$$', '$$'],
       ['\\[', '\\]']
     ],
-    {%- comment -%} equation numbering {%- endcomment -%}
-    tags: 'ams'
-  }
+    packages: {'[+]': ['physics']},
+    tags: 'ams',
+    macros: {
+        'e': '\\mathrm{e}',
+        'i': '\\mathrm{i}',
+        'RR': '\\mathbb{R}',
+        'ZZ': '\\mathbb{Z}',
+        'QQ': '\\mathbb{Q}',
+      },
+  },
+  svg: { fontCache: 'global'},
 };
